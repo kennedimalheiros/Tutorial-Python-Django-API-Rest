6af6d76c7483845eccf82ac963fb77a04d63a12c
kennedimalheiros <kennedimalheiros@gmail.com>
2020-07-21 23:03:07 -0300

    modify class name

--- product/urls.py
+++ product/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
-from .views import productList
+from .views import ProductView
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register('', productList)
+router.register('', ProductView)
 
 urlpatterns = [
     path('', include(router.urls))
