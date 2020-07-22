4225386cca93a6b10dc7cd691e90f774b2c2978e
kennedimalheiros <kennedimalheiros@gmail.com>
2020-07-21 23:14:12 -0300

    Creating view productView

--- product/views.py
+++ product/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Product
+from .serializers import ProductSerializer
 
 
 def home(request):
     return render(request, 'index.html')
+
+
+class ProductView(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
