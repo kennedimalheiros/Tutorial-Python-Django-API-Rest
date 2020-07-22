609cb0d4cdee6e1499cfef4220dd52e651bfecfd
kennedimalheiros <kennedimalheiros@gmail.com>
2020-07-21 23:09:03 -0300

    creating route to product app

--- pdv/urls.py
+++ pdv/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from product.views import home
 
 urlpatterns = [
     path('', home),
+    path('product/', include('product.urls')),
     path('admin/', admin.site.urls),
 ]
